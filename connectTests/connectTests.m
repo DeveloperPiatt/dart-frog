56de5fa3b9d4a1dedf1f51cfa8be7ef5e9e92a23
DeveloperPiatt <developer.piatt@gmail.com>
2014-01-21 16:07:16 -0800

    Initial Code Commit

---
+++ connectTests/connectTests.m
@@ -0,0 +1,34 @@
+//
+//  connectTests.m
+//  connectTests
+//
+//  Created by NickPiatt on 1/21/14.
+//  Copyright (c) 2014 Oregon State University. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface connectTests : XCTestCase
+
+@end
+
+@implementation connectTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end

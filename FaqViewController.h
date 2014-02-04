46ef37e807eab21daea287db51e647ba367f49f1
Taylor Cuilty <cuiltyt@onid.orst.edu>
2014-02-03 16:12:24 -0800

    CON-3 #comment Created new branch for FAQ view controller #time 10m

---
+++ FaqViewController.h
@@ -0,0 +1,13 @@
+//
+//  FaqViewController.h
+//  connect
+//
+//  Created by Taylor Cuilty on 2/3/14.
+//  Copyright (c) 2014 Oregon State University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FaqViewController : UITableViewController
+
+@end

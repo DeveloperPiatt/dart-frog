b880a3bcdfe02bec827f98fe1baf9a96c654431e
Taylor Cuilty <cuiltyt@onid.orst.edu>
2014-02-24 14:55:45 -0800

    CON-27 #comment Implemented functions for connecting to web #time 20m

---
+++ ContactsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactsViewController.h
+//  connect
+//
+//  Created by Taylor Cuilty on 2/24/14.
+//  Copyright (c) 2014 Oregon State University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactsViewController : UITableViewController <UITableViewDataSource, UITableViewDelegate, NSURLConnectionDataDelegate, NSFetchedResultsControllerDelegate>
+
+@end
